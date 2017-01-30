b3fe63a885073b43e045467362b4d78c1e1e0e60
Scott Umsted <scottumsted@gmail.com>
2017-01-30 06:52:32 -0600

    updating check scripts

--- scripts/check_roboteye.sh
+++ scripts/check_roboteye.sh
@@ -3,5 +3,5 @@ if ps -ef | grep "roboteye.py" | grep -v grep >/dev/null 2>&1; then
     exit 0
 else
     echo "**** starting roboteye  ****"
-    nohup /home/pi/webapps/roboteye/roboteys/scripts/start_roboteye.sh > /home/pi/projects/logs/nohup_roboteye.out 2> /home/pi/projects/logs/nohup_roboteye.err < /dev/null &
+    nohup /home/pi/webapps/roboteye/roboteye/scripts/start_roboteye.sh > /home/pi/projects/logs/nohup_roboteye.out 2> /home/pi/projects/logs/nohup_roboteye.err < /dev/null &
 fi
